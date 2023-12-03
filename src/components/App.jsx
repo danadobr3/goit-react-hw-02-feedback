bf4ee5b0f5a2abe4690bbd3cc451245a336ddff7
danadobr3 <134941956+danadobr3@users.noreply.github.com>
2023-12-03 22:43:05 +0100

    redoo

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,14 +7,11 @@ import { Section } from "./Section/Section";
 
 
 export class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      good: 0,
-      neutral: 0,
-      bad: 0
-    };
-  }
+   state = {
+    good: 0,
+    neutral: 0,
+    bad: 0
+  };
 
   countTotalFeedback = () => {
     return this.state.good + this.state.neutral + this.state.bad;
